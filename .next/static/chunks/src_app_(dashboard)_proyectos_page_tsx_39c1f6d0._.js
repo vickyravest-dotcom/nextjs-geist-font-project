dbe2e0a732013ca35c1700aac4314bd384f88098
vickyravest-dotcom <vickyravest@gmail.com>
2025-08-09 21:28:23 -0400

    Add .next/static/chunks/src_app_(dashboard)_proyectos_page_tsx_39c1f6d0._.js 1754789301754

---
+++ .next/static/chunks/src_app_(dashboard)_proyectos_page_tsx_39c1f6d0._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_(dashboard)_proyectos_page_tsx_96210c56._.js"
+],
+    source: "dynamic"
+});
